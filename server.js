67458d54d819e7ba9435367fac8979642884bee2
16kbf@queensu.ca <33009041+kaiferrall@users.noreply.github.com>
2018-11-22 17:04:12 -0500

    added path import

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const bodyParser = require("body-parser");
 //Deprecation for option parameters in queries. Currently using an older version of mongoose
 //remember to update
